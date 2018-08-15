d739b7e9d6bc4ef5db321be76bf5beee828051c1
Scott Sawyer <38329255+scottasawyer@users.noreply.github.com>
2018-08-14 23:58:38 -0400

    initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,24 @@
+$(function () {
+
+    $(document).scroll(function () {
+        if ($(window).scrollTop() >= $('header').offset().top) {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    });
+
+    $('body').scrollspy({
+        target: '.navbar',
+        offset: 80
+    });
+
+    $('.navbar-nav a, #scroll-down').bind('click', function (e) {
+        var anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $(anchor.attr('href')).offset().top
+        }, 1000);
+        e.preventDefault();
+    });
+
+});
