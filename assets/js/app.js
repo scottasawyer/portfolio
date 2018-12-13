13c817170439ea7f116d890917306d83115d7b65
Scott Sawyer <38329255+scottasawyer@users.noreply.github.com>
2018-12-12 22:44:02 -0500

    Added styling and projects

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,3 +22,35 @@ $(function () {
     });
 
 });
+
+$(document).ready(function () {
+    $("a.scrollLink").click(function (event) {
+        event.preventDefault();
+        $("html, body").animate({
+            scrollTop: $($(this).attr("href")).offset().top
+        }, 500);
+    });
+});
+
+const chevron = document.querySelector('i');
+
+function animateChevron(scale, duration, elasticity) {
+    anime.remove(chevron);
+    anime({
+        targets: chevron,
+        scale: scale,
+        duration: duration,
+        elasticity: elasticity
+    });
+}
+
+function enterButton() {
+    animateChevron(1.2, 800, 400)
+};
+
+function leaveButton() {
+    animateChevron(1.0, 600, 300)
+};
+
+chevron.addEventListener('mouseenter', enterButton, false);
+chevron.addEventListener('mouseleave', leaveButton, false);
